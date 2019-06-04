c8f2152a0db97d6f07614abc8fe0dc83210b7752
Martin Nerurkar <mnerurkar@sharkbombs.com>
2019-06-04 10:22:53 +0200

    test weapon added

---
+++ Unity/Assets/Scenes/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : ScriptableObject
+{
+
+    public virtual void Fire(Vector3 projectileOrigin, Vector2 direction) {
+        Debug.Log("Pew (" + name + ")");
+    }
+
+}
---
+++ Unity/Assets/Scenes/Scripts/Weapons/WeaponTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapons/Test", fileName = "New Weapon Test")]
+public class WeaponTest : Weapon
+{
+    public override void Fire(Vector3 projectileOrigin, Vector2 direction) {
+        base.Fire(projectileOrigin, direction);
+    }
+}
